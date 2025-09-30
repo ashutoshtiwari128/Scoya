914e76576bbde13e090e2751f5d21de50a84383a
Ashutosh Tiwari <47528921+ashutoshtiwari128@users.noreply.github.com>
2025-10-01 01:25:28 +0530

    Setup initial project structure and Players definition (#1)

---
+++ src/core/config.py
@@ -0,0 +1,18 @@
+from transport_type import TransportType
+
+MAXIMUM_MOVES = 25
+
+NUMBER_OF_2X_TICKETS = 2
+
+DETECTIVE_INITIAL_TICKETS: dict[TransportType, int] = {
+    TransportType.TAXI: 10,
+    TransportType.BUS: 8,
+    TransportType.UNDERGROUND: 4
+}
+
+MRX_INITIAL_TICKETS: dict[TransportType, int] = {
+    TransportType.BLACK: 5,
+    TransportType.TAXI: MAXIMUM_MOVES,
+    TransportType.BUS: MAXIMUM_MOVES,
+    TransportType.UNDERGROUND: MAXIMUM_MOVES
+}
---
+++ src/core/players/detective.py
@@ -0,0 +1,9 @@
+from player import Player
+from config import DETECTIVE_INITIAL_TICKETS
+
+class Detective(Player):
+    """
+    Class of a detective player. The goal of detective is to catch Mr. X by making valide moves on each turn.
+    """
+    def __self__(self, name: str, initial_position: int):
+        super().__init__(name=name, initial_position=initial_position, initial_tickets=DETECTIVE_INITIAL_TICKETS)
